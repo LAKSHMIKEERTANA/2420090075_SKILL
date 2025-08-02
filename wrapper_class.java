6b4e23a88ccdde50beff9287f8f37050b48cbc16
LAKSHMIKEERTANA <2420090075@klh.edu.in>
2025-08-02 09:15:52 +0530

    Add files via upload

---
+++ wrapper_class.java
@@ -0,0 +1,15 @@
+package myproject;
+
+public class wrapper_class {
+	public static void main(String args[]) {
+		Integer a=569;
+		Boolean b=true;
+		Double d=9.876d;
+		int c=10;
+		System.out.println(a.toString().length());
+		System.out.println(d.toString().length());
+
+		
+	}
+
+}
