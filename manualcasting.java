17c6f1b095d84b77da06ed444003f7fb48e891a0
LAKSHMIKEERTANA <2420090075@klh.edu.in>
2025-08-02 09:21:52 +0530

    Add files via upload

---
+++ manualcasting.java
@@ -0,0 +1,10 @@
+package myproject;
+
+public class manualcasting {
+	public static void main(String arg[]) {
+		int a= 9;
+		double d=a;
+		System.out.println("d value "+d);
+	}
+
+}
---
+++ manualconversion.java
@@ -0,0 +1,10 @@
+package myproject;
+
+public class manualconversion {
+	public static void main(String args[]) {
+		double d=9.5;
+		int a=(int)d;
+		System.out.println("d "+a);
+	}
+
+}
